f8f7fd4daa750570ed67ac348fe229685b3ee348
Rafael Furry and tyler miller <rafael@>
2017-08-09 11:12:47 -0700

    adds subtract, multiply and divide buttons

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,30 @@ var divide = function(number1, number2) {
 // User inter logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#math").submit(function(event) {
     event.preventDefault();
+    var result = 0;
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
 
-    var result = add(number1, number2);
-    $("#output").text(result);
+    $("#b1").click(function(event) {
+      result = add(number1, number2);
+      $("#output").text(result);
+      alert(result);
+    })
+    $("#b2").click(function(event) {
+      result = subtract(number1, number2);
+      $("#output").text(result);
+    })
+    $("#b3").click(function(event) {
+      result = multiply(number1, number2);
+      $("#output").text(result);
+    })
+    $("#b4").click(function(event) {
+      result = divide(number1, number2);
+      $("#output").text(result);
+    })
+
+
   });
 });
